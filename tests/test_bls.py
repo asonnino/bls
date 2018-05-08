082bbbf79d9a4dce97aa08a6dd10cfd2b3c97204
asonnino <alberto.sonnino@ucl.ac.uk>
2018-05-09 00:37:49 +0100

    Implementation

---
+++ tests/test_bls.py
@@ -0,0 +1,47 @@
+from bls.scheme import *
+
+
+def test_threshold_authorities():
+	m = [3] * 2 # messages
+	t, n = 2, 3 # number of authorities
+	params = setup()
+
+	# generate key
+	(sk, vk) = ttp_keygen(params, t, n)
+
+	# aggregate verification keys
+	aggr_vk = aggregate_vk(params, vk)
+
+	# bind sign
+	sigs = [sign(params, ski, m) for ski in sk]
+
+	# aggregate credentials
+	sigma = aggregate_sigma(params, sigs)
+
+	# verify signature
+	assert verify(params, aggr_vk, sigma, m)
+
+
+def test_multi_authorities():
+	m = [3] * 2 # messages
+	n = 3 # number of authorities
+	params = setup()
+
+	# generate key
+	(sk, vk) = ttp_keygen(params, n, n)
+
+	# aggregate verification keys
+	aggr_vk = aggregate_vk(params, vk, threshold=False)
+
+	# bind sign
+	sigs = [sign(params, ski, m) for ski in sk]
+
+	# aggregate credentials
+	sigma = aggregate_sigma(params, sigs, threshold=False)
+
+	# verify signature
+	assert verify(params, aggr_vk, sigma, m)
+
+
+
+	
