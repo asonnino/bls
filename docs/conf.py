d4991cb3d36be9e11cdff42fa4de7996db3c1428
asonnino <alberto.sonnino@ucl.ac.uk>
2018-05-23 12:57:31 +0100

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,6 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('..'))
 
-import bls
 
 
 # -- Project information -----------------------------------------------------
